3fa826d64af231880af48af4ff714407de4f7ab5
alphaR0ut <gero.lindner@protonmail.com>
2022-03-03 21:49:03 +0100

    Added ConfigManager, added message.yml

--- src/main/java/de/alpharout/ownworlds/OwnWorlds.java
+++ src/main/java/de/alpharout/ownworlds/OwnWorlds.java
@@ -1,15 +1,33 @@
 package de.alpharout.ownworlds;
 
+import de.alpharout.ownworlds.listener.JoinListener;
+import de.alpharout.ownworlds.utils.ConfigManager;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class OwnWorlds extends JavaPlugin {
+    private static OwnWorlds PLUGIN;
+    private static ConfigManager CONFIGMANAGER;
+
     @Override
     public void onEnable() {
+        PLUGIN = this;
+        CONFIGMANAGER = new ConfigManager();
 
+        CONFIGMANAGER.load();
     }
 
     @Override
     public void onDisable() {
 
+    }
+
+    public static OwnWorlds getInstance() {
+        return PLUGIN;
+    }
+
+    public static ConfigManager getConfigManager() {
+        return CONFIGMANAGER;
     }
 }
---
+++ src/main/java/de/alpharout/ownworlds/utils/ConfigManager.java
@@ -0,0 +1,37 @@
+package de.alpharout.ownworlds.utils;
+
+import de.alpharout.ownworlds.OwnWorlds;
+import org.bukkit.configuration.InvalidConfigurationException;
+import org.bukkit.configuration.file.YamlConfiguration;
+
+import java.io.File;
+import java.io.IOException;
+
+public class ConfigManager {
+    private File messageFile;
+    private YamlConfiguration messageConf;
+
+    public void load() {
+        messageFile = new File(OwnWorlds.getInstance().getDataFolder(), "message.yml");
+
+        if (!messageFile.exists()) {
+            OwnWorlds.getInstance().saveResource("message.yml", false);
+        }
+        messageConf = new YamlConfiguration();
+        try {
+            messageConf.load(messageFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InvalidConfigurationException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public File getMessageFile() {
+        return messageFile;
+    }
+
+    public YamlConfiguration getMessageConf() {
+        return messageConf;
+    }
+}
